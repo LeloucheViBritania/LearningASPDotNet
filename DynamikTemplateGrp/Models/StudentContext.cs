e286114f40d330f4d2535ff60252bcb1b94f501a
LeloucheViBritania <kra.kobenan.olivier@gmail.com>
2020-07-17 11:35:28 +0000

    Ajout des models pour la database

---
+++ DynamikTemplateGrp/Models/StudentContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DynamikTemplateGrp.Models
+{
+    public class StudentContext : DbContext
+    {
+        private DbSet<Categorie> _categorie;
+
+        public StudentContext()
+: base("name=StudentContext")
+        {
+        }
+
+        public DbSet<Student> student { get; set; }
+        public DbSet<Cours> cours { get; set; }
+        public DbSet<Follow> follow { get; set; }
+        public DbSet<Categorie> Categorie { get => _categorie; }
+    }
+}
